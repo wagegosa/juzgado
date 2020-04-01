cea22f04dafaab23f96304a62f8ffeda1b7d0e50
Gerardo González <wagegosa@gmail.com>
2020-04-01 17:20:25 -0500

    Se crea formulario para agregar la naturaleza.

---
+++ admin/configuracion/naturaleza/New_Naturaleza.php
@@ -0,0 +1,63 @@
+<?php  
+session_start();
+if(!empty($_SESSION['active']) && $_SESSION['perfil'] === "1"){
+  $alert = 'Se <strong>Almacenaron</strong> los datos corrrectamente';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <!--Bootstrap núcleo CSS-->
+  <link rel="stylesheet" media="screen" href="../../../css/assets/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" media="screen" href="../../../css/assets/bootstrap/css/bootstrap.min.css">
+  <!--Biblioteca de iconos monocromáticos y símbolos-->
+  <link rel="stylesheet" href="../../../css/assets/bootstrap/fonts/glyphicons-pro/css/glyphicons-pro.css">
+  <link rel="stylesheet" href="../../../css/assets/bootstrap/fonts/font-awesome/css/font-awesome.min.css">
+  <title>Naturaleza</title>
+</head>
+<body>
+  <div class="container">
+    <?php include "../../../plantillas/menu/menu_admin2.php"; 
+    if($_GET != null){?>
+      <div class="alert alert-success"><?php echo isset($alert) ? $alert : ''; ?></div>
+    <?php } ?>
+    <div class="row">
+      <div class="col-md-12">
+        <h3 class="page-header"><span class="glyphicons glyphicons-group"></span> Naturaleza</h3>
+        <ol class="breadcrumb">
+          <li><a href="">Inicio</a></li>
+          <li><a href="">Configuraciones</a></li>
+          <li><a href="">Naturaleza</a></li>
+          <li class="active">Nueva Naturaleza</li>
+        </ol>
+      </div>
+    </div>
+    <form action="../../../config/ClassNaturaleza/ClassNaturaleza_Ins.php" class="frm" id="frm" autocomplete="off" method="post">
+      <div class="row form-group">
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+          <label for="nombre" >Nombre</label>
+        </div>
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+          <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre" require>
+        </div>
+      </div>
+      <div class="row form-group">
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+          <label for="nombre">Activo</label>
+        </div>
+        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+          <label for="Y" class="radio-inline"><input type="radio" name="activo" id="activo" value="Y" checked> Si</label>
+          <label for="N" class="radio-inline"><input type="radio" name="activo" id="activo" value="N" disabled> No</label>
+        </div>
+      </div>
+      <div class="row form-group">
+        <button type="submit" class="btn btn-primary btn-lg btn-block">Guardar</button>
+      </div>
+    </form>
+  </div>
+  <!-- LIBRERIAS validadoras-->
+  <script src="../../css/assets/js/plugins/jquery/jquery-3.2.1.min.js"></script>
+  <script src="../../css/assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
+<?php } ?>
