6c120dcb8aa71a5da0481b90e80d7c8c9f65bb2f
Gerardo González <wagegosa@gmail.com>
2020-04-03 15:13:56 -0500

    Se crea funciones de consulta de listar Novedad y listar novedad activo este ultimo para ver en un select.

--- config/ClassNovedad/ClassNovedad_sel.php
+++ config/ClassNovedad/ClassNovedad_sel.php
@@ -1,16 +1,16 @@
  <?php
 
-class Lugar extends DataBase
+class Novedad extends DataBase
 {
-	public $idtbp_lugares_votacio;
+	public $id_novedad;
   public $nombre;
 	public $activo;
 
-  public function listarLugar(){
+  public function listarNovedad(){
     try
     {
       parent::Conexion();
-      $sql = "SELECT * FROM gloriadiaz.tbp_lugares_votacio WHERE activo = 'Y' ORDER BY nombre ASC";
+      $sql = "SELECT * FROM novedad ORDER BY nombre ASC";
       $qry = $this->dbCon->prepare($sql);
       $qry->execute();
       $row = $qry->fetchAll(PDO::FETCH_OBJ);
@@ -22,8 +22,22 @@ class Lugar extends DataBase
     {
       die("Ha ocurrido un error inesperado en la base de datos.<br>".$e->getMessage());
     }
+  } 
+  public function ListarNovedadAct(){
+    try{
+      parrent::Conexion();
+      $sql = "SELECT * FROM novedad  WHERE activo = 'Y' ORDER BY nombre ASC";
+      $qry = $this->dbCon->prepare($sql);
+      $qry->execute;
+      $row = $qry->fetchAll(PDO::FETCH_OBJ);
+      $qry->closeCursor();
+      return $row;
+      $this->dbCon = null;
+    }
+    catch ( PDOException $e )
+    {
+      die("Ha ocurrido un error inesperado en la base de datos.<br>".$e->getMessage());
+    }
   }
-  
 }
-
 ?>
