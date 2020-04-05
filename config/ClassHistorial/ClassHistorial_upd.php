25a108da019bdecc978aa24a63a38a95e911b504
Gerardo González <wagegosa@gmail.com>
2020-04-04 23:27:02 -0500

    Se crea archivo de clase para realizar actualizacion a datos en la DB.

--- config/ClassHistorial/ClassHistorial_upd.php
+++ config/ClassHistorial/ClassHistorial_upd.php
@@ -1,31 +1,31 @@
  <?php
- date_default_timezone_set('America/Bogota');
- $hoy = date('Y-m-d');
 //llamamos a la connecion
 require('../General/connexion.php');
 //capturamos
-$num_cedula= $_POST['num_cedula'];
-$nombre= $_POST['nombre'];
-$telefono= $_POST['telefono'];
-$email= $_POST['email'];
-$direccion= $_POST['direccion'];
-$localidad= $_POST['localidad'];
-$id= $_POST['id'];
-if($localidad == 0){
-  $localidad = "21";
-}
-$lugar_votacio = $_POST['lugar_votacio'];
-if($lugar_votacio == 0){
-  $lugar_votacio = "643";
-}
-$organizador = $_POST['organizador'];
+$naturaleza= $_POST['naturaleza'];
+$consecutivo= $_POST['id'];
+$demandante= $_POST['demandante'];
+$demandado= $_POST['demandado'];
+$fec_reparto= $_POST['fec_reparto'];
+$fec_termina= $_POST['fec_termina'];
+$novedad= $_POST['novedad'];
+$archivo = $_POST['archivo'];
 if($_POST != "" ){
   try{
     $Con= new DataBase();
     $Conexion= $Con->Conexion();
-    $query = "UPDATE gloriadiaz.tba_regist_asiste SET cedula= '$num_cedula', nom_comple= '$nombre', telefono= '$telefono', correo= '$email', dir_reside= '$direccion', localida= '$localidad', lugar_votacio= '$lugar_votacio', usuario= '1',  organizador= '$organizador', fec_modifi= '$hoy' WHERE idtba_regist_asiste = $id";
+    $query = "UPDATE historial 
+                 SET naturaleza = '$naturaleza', demandante = '$demandante',
+                     demandado = '$demandado', fe_reparto = '$fec_reparto',
+                     fe_terminacion = '$fec_termina', novedad = '$novedad',
+                     archivo = '$archivo' 
+                WHERE consecutivo= ".$consecutivo.";";
+// echo "<pre>";
+// print_r($query);
+// echo "</pre>";
+// die();
     $Conexion->query($query);
-    header("Location: ../../admin/asistentes/Con_Asistencia.php?c=1");
+    header("Location: ../../admin/historial/Con_historial.php?c=1");
   }
   catch ( PDOException $e ){
     echo "<script>alert('¡Por favor revisar los datos ingresados, estos no pueden estar vacíos! ');</script>";
