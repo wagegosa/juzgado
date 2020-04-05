6018378762cb532de6ff5c4b93a0c1c79c500dd8
Gerardo González <wagegosa@gmail.com>
2020-04-04 23:29:21 -0500

    Se crea formulario para poder editar o actualizar informacion del historial de un proceso.

--- admin/historial/Edi_historial.php
+++ admin/historial/Edi_historial.php
@@ -1,36 +1,35 @@
 <?php 
-  session_start();
-?>
-<?php
-//Conexión a la base de datos
-require "../../config/General/connexion.php";
+session_start();
+if(!empty($_SESSION['active']) && $_SESSION['perfil'] === "1"){
+  //Conexión a la base de datos
+  require "../../config/General/connexion.php";
+  include "../../config/ClassNaturaleza/ClassNaturaleza_sel.php";
+  include '../../config/ClassNovedad/ClassNovedad_sel.php';
+  $natu = new Naturaleza();
+  $nove = new Novedad();
+  $naturaleza = $natu->listarNaturaleza();
+  $novedad = $nove->ListarNovedadAct();
 
-include "../../config/ClassAsistencia/ClassAsistencia_sel.php";
-include "../../config/ClassLocalidad/ClassLocalidad_sel.php";
-include "../../config/ClassLugar/ClassLugar_sel.php";
-$asistencia = new Asistencia();
-$asis    = $asistencia->listarAsistenciaActi();
-$localidad = new Localidad();
-$local    = $localidad->listarLoocalidadActi();
-$lugar = new Lugar();
-$lugar_vota = $lugar->listarLugar();
+  $Con= new DataBase();
+  $Conn= $Con->Conexion();
 
-$Con= new DataBase();
-$Conn= $Con->Conexion();
+  $_GET['id'] ? $_GET['id'] : '';
 
-$_GET['id'] ? $_GET['id'] : '';
+  try {
+    $query="SELECT * FROM historial where consecutivo = ".$_GET['id'].";";
+// echo "<pre>";
+// print_r($query);
+// echo "</pre>";
+// die();
+    $Resul= $Conn->prepare($query);
+    $Resul->bindParam(':id', $id);
+    $Resul->execute();
+    $Resul->setFetchMode(PDO::FETCH_ASSOC);
+    // $Resul->setFetchMode(PDO::FETCH_ASSOC);
+    $Resultado= $Resul->rowCount();
 
-try {
-  $query="SELECT * FROM gloriadiaz.tba_regist_asiste WHERE idtba_regist_asiste = ".$_GET['id'];
-  $Resul= $Conn->prepare($query);
-  $Resul->bindParam(':id', $id);
-  $Resul->execute();
-  $Resul->setFetchMode(PDO::FETCH_ASSOC);
-  // $Resul->setFetchMode(PDO::FETCH_ASSOC);
-  $Resultado= $Resul->rowCount();
-
-  if($Resultado!=0){
-    while ($R= $Resul->fetch()):
+    if($Resultado!=0){
+      while ($R= $Resul->fetch()):
 ?>
 <!DOCTYPE html>
 <html lang="es" ng-app> 
@@ -56,158 +55,150 @@ try {
   <div class="container">
     <?php include "../../plantillas/menu/menu_admin2.php"; ?>
     <div class="row">
-      <div class="col-md-12">
-        <h3 class="page-header"><span class="glyphicons glyphicons-group"></span> Asistentes</h3>
-        <ol class="breadcrumb">
-          <li><a href="">Inicio</a></li>
-          <li><a href="Con_Asistencia.php">Asistentes</a></li>
-          <li class="active">Nuevo Asistente</li>
-        </ol>
+        <div class="col-md-12">
+          <h3 class="page-header"><span class="glyphicons glyphicons-group"></span> Historial</h3>
+          <ol class="breadcrumb">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Historial</a></li>
+            <li class="active">Nuevo Historial</li>
+          </ol>
+        </div>
       </div>
-    </div>
-    <!-- contador -->
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <span class="user">Asistencias Resgistradas: <?php foreach ($asis as $listarU): echo $listarU->contador; endforeach;?> </span>
-      </div>
-    </div><br>  
     <!-- Formulario -->
-    <form method="post" autocomplete="on" id="frm" action="../../config/ClassAsistencia/ClassAsistencia_upd.php?c=<?=$_GET['id'];?>">
-      <!-- Cedula -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="Nombre">Cedula:</label>
+      <form method="post" autocomplete="on" id="frm" action="../../config/ClassHistorial/ClassHistorial_upd.php">
+        <!-- Natura -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="email">Naturaleza:</label>
+          </div>
+          <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+            <select name="naturaleza" id="naturaleza" class="form-control select2" required="required">
+              <option value="0">Seleccione... </option>
+              <?php foreach ($naturaleza as $listarN): ?>
+                <?php if($R['naturaleza'] == $listarN->id_naturaleza){ ?>
+                  <option value="<?= $listarN->id_naturaleza;?>" selected><?= $listarN->nombre;?></option>
+                <?php }else{ ?>
+                  <option value="<?= $listarN->id_naturaleza;?>"><?= $listarN->nombre;?></option>
+              <?php }
+            endforeach; ?>
+            </select>
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <input type="number" class="form-control" name="num_cedula" rows="5" id="num_cedula" minlength="7" maxlength="10" placeholder="Cedula" value="<?=$R['cedula'];?>">
-          <span class="help-block" id="error"></span>
+        <!-- Consecutivo -->
+        <!-- <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="Hora">Consecutiv:o</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <input type="text" class="form-control" name="consecutivo" id="consecutivo" placeholder="Consecutivo" required="required">
+            <span class="help-block" id="error"></span>
+          </div>
+        </div> -->
+        <!-- Demandante -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="email">Demandante:</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <input type="text" class="form-control" name="demandante" id="demandante" placeholder="Demandante" minlength="7" maxlength="120" required="required" value="<?= $R['demandante']; ?>">
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-      </div>
-      <!-- Nombre -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="Hora">Nombre Completo</label>
+        <!-- Demandado -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="email">Demandado:</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <input type="text" class="form-control" name="demandado" id="demandado" placeholder="Demandado" minlength="7" maxlength="120" required="required" value="<?= $R['demandado']; ?>">
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre Completo" value="<?=$R['nom_comple'];?>">
-          <span class="help-block" id="error"></span>
+        <!-- Fecha Reparto -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="direccion">Fecha Reparto:</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <input type="date" class="form-control" name="fec_reparto" id="fec_reparto" placeholder="Fecha Reparto" required="required" value="<?= $R['fe_reparto'];?>">
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-      </div>
-      <!-- telefono -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="email">Telefono:</label>
+        <!-- Fecha Terminación -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="fec_termina">Fecha Terminación:</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <input type="date" class="form-control" name="fec_termina" id="fec_termina" placeholder="Fecha Terminación" value="<?= $R['fe_terminacion'];?>">
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <input type="number" class="form-control" name="telefono" id="telefono" placeholder="Telefono" minlength="7" maxlength="10">
-          <span class="help-block" id="error"></span>
+        <!-- Novedad -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="novedad">Novedad:</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <select name="novedad" id="novedad" class="form-control select2" required="required">
+              <option value="0">Seleccione... </option>
+              <?php foreach ($novedad as $listarL): ?>
+                <?php if($R['novedad'] == $listarL->id_novedad){ ?>
+                  <option value="<?= $listarL->id_novedad;?>" selected><?= $listarL->nombre;?></option>
+                <?php }else{ ?>
+                  <option value="<?= $listarL->id_novedad;?>"><?= $listarL->nombre;?></option>
+              <?php }
+            endforeach; ?>
+            </select>
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-      </div>
-      <!-- Email -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="email">email:</label>
+        <!-- Archivo -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            <label for="archivo">Archivo:</label>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+            <input type="text" class="form-control" name="archivo" id="archivo" placeholder="Archivo" value="<?= $R['archivo']; ?>">
+            <span class="help-block" id="error"></span>
+          </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="<?=$R['correo'];?>">
-          <span class="help-block" id="error"></span>
-        </div>
-      </div>
-      <!-- Dirección -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="direccion">Dirección:</label>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <input type="text" class="form-control" name="direccion" id="direccion" placeholder="Dirección" value="<?=$R['dir_reside'];?>">
-          <span class="help-block" id="error"></span>
-        </div>
-      </div>
-      <!-- localidad -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="Localidad">Localidad:</label>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <select name="localidad" id="localidad" class="form-control select2">
-            <option value="0">Seleccione... </option>
-            <?php foreach ($local as $listarL): 
-                if ($listarL->tbp_localida != $R['localida']) {?>
-                  <option value="<?= $listarL->tbp_localida;?>"><?= $listarL->nombre;?></option>
-              <?php
-                }else{
-                  echo "<option value='$listarL->tbp_localida' selected>$listarL->nombre</option>";
-                }
-               ?>
-              <?php endforeach; ?>
-          </select>
-          <span class="help-block" id="error"></span>
-        </div>
-      </div>
-      <!-- Lugar Votación -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="Localidad">Lugar Votación:</label>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <select name="lugar_votacio" id="lugar_votacio" class="form-control select2">
-            <option value="0">Seleccione... </option>
-              <?php foreach ($lugar_vota as $listarV): 
-                if ($listarV->idtbp_lugares_votacio != $R['lugar_votacio']) {
-                  echo "<option value='$listarV->idtbp_lugares_votacio'>$listarV->nombre</option>";
-                }else{
-                  echo "<option value='$listarV->idtbp_lugares_votacio' selected>$listarV->nombre</option>";
-                }
-                endforeach; 
-              ?>
-          </select>
-          <span class="help-block" id="error"></span>
-        </div>
-      </div>
-      <!-- ORGANIZADOR -->
-      <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-          <label for="direccion">Organizador:</label>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-          <input type="text" class="form-control" name="organizador" id="organizador" placeholder="Organizador" value="<?=$R['organizador'];?>">
-          <span class="help-block" id="error"></span>
-        </div>
-      </div>
-      <div><br>
-        <div><br>
-          <input type="hidden" name="id" id="id" value="<?= $R['idtba_regist_asiste']?>">
-          <button type="submit" class="btn btn-primary btn-lg btn-block">Guardar</button><br>
+        <!-- Boton -->
+        <div><br> 
+          <input type="hidden" name="id" id="id" value="<?=$R['consecutivo'] ?>">
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Guardar</button>
         </div>
       </form>
-  </div>
-  <!-- LIBRERIAS validadoras-->
-  <script src="../../css/assets/js/plugins/jquery/jquery-3.2.1.min.js"></script>
-  <script src="../../css/assets/bootstrap/js/bootstrap.min.js"></script>
-  <!-- Plugin para la validación de formularios -->
-  <script src="../../css/assets/jquery_validation/dist/jquery.validate.min.js"></script>
-  <script src="../../css/assets/jquery_validation/dist/localization/messages_es.js"></script>
-  <!-- Plugin para listado, navegación y filtrado en tablas -->
-  <script src="../../css/assets/footable/js/footable.min.js"></script>
-  <script src="../../css/assets/footable/js/configTable.js"></script>
-  <!-- Plugin para cuadro de selección personalizable con soporte para búsqueda. -->
-  <script src="../../js/plugins/select2/select2.full.js"></script>
-  <script src="../../js/plugins/select2/es.js"></script>
-  <script>
-    $(document).ready(function() {
-     $(".select2").select2({
-      language: "es"
-    });
-   });
- </script>
-</body>
-</html>
+    </div>
+    <!-- LIBRERIAS validadoras-->
+    <script src="../../css/assets/js/plugins/jquery/jquery-3.2.1.min.js"></script>
+    <script src="../../css/assets/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin para la validación de formularios -->
+    <script src="../../css/assets/jquery_validation/dist/jquery.validate.min.js"></script>
+    <script src="../../css/assets/jquery_validation/dist/localization/messages_es.js"></script>
+    <!-- Plugin para listado, navegación y filtrado en tablas -->
+    <script src="../../css/assets/footable/js/footable.min.js"></script>
+    <script src="../../css/assets/footable/js/configTable.js"></script>
+    <!-- Plugin para cuadro de selección personalizable con soporte para búsqueda. -->
+    <script src="../../js/plugins/select2/select2.full.js"></script>
+    <script src="../../js/plugins/select2/es.js"></script>
+    <script>
+      $(document).ready(function() {
+        $(".select2").select2({
+          language: "es"
+        });
+     });
+    </script>
+  </body>
+  </html>
 <?php    
-    endwhile;
-  }else{
-    echo "No se encontraron registros con el ID " .$id;
+      endwhile;
+    }else{
+      echo "No se encontraron registros con el ID " .$id;
+    }
+  } catch (PDOException $e) {
+    die("Error occurred:" . $e->getMessage());
   }
-} catch (PDOException $e) {
-  die("Error occurred:" . $e->getMessage());
 }
 ?>
